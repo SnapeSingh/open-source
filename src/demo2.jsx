6443bc670762a1c2b318fca6ce19b23e9c03dbf0
prajjwal98dubey <prajjwaldubey003@gmail.com>
2024-10-05 14:54:54 +0530

    commit on demo2 by p98d

--- src/demo2.jsx
+++ src/demo2.jsx
@@ -1,6 +1,6 @@
 function Demo2(){
     return (
-        <h1>this is demo made by kbs in main branch.</h1>
+        <h1>this is demo made by kbs in main branch and now it is updated by p98d.</h1>
     )
 }
 export default Demo2
